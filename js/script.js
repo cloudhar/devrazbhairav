d665bfb74a12ab5bb2f9545e72388ad698b7725a
manikpandita23 <115569552+manikpandita23@users.noreply.github.com>
2024-07-25 00:17:08 +0530

    feat: Created separate view for mobile view(show more/show less) and removed the functionality for desktop view

--- js/script.js
+++ js/script.js
@@ -1,33 +1,26 @@
-
 document.addEventListener('DOMContentLoaded', function () {
     // JavaScript is not necessary for this basic example as the link navigates to another page.
     // Additional JavaScript functionality can be added if needed.
 });
 
-/*	gallery */
+/* gallery */
 $(document).ready(function(){
-
     $(".filter-button").click(function(){
         var value = $(this).attr('data-filter');
         
-        if(value == "all")
-        {
+        if(value == "all") {
             $('.filter').show('1000');
-        }
-        else
-        {
+        } else {
             $(".filter").not('.'+value).hide('3000');
             $('.filter').filter('.'+value).show('3000');
-            
         }
 
-	        	if ($(".filter-button").removeClass("active")) {
-			$(this).removeClass("active");
-		    }
-		    	$(this).addClass("active");
-	    	});
+        if ($(".filter-button").removeClass("active")) {
+            $(this).removeClass("active");
+        }
+        $(this).addClass("active");
+    });
 });
-/*	end gallery */
 
 $(document).ready(function(){
     $(".fancybox").fancybox({
@@ -35,5 +28,26 @@ $(document).ready(function(){
         closeEffect: "none"
     });
 });
-   
-  
+
+//Mobile view
+function toggleText(button) {
+    const hiddenText = button.previousElementSibling.querySelector('.hidden-text');
+
+    if (hiddenText.style.display === 'none' || hiddenText.style.display === '') {
+        hiddenText.style.display = 'inline';
+        button.textContent = 'Show Less';
+    } else {
+        hiddenText.style.display = 'none';
+        button.textContent = 'Show More';
+    }
+}
+
+// Mobile View
+document.addEventListener('DOMContentLoaded', function () {
+    if (window.innerWidth <= 767) {
+        const hiddenTexts = document.querySelectorAll('.hidden-text');
+        hiddenTexts.forEach(hiddenText => {
+            hiddenText.style.display = 'none';
+        });
+    }
+});
